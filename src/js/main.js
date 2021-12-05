e70695c4cf9e3cb9f189f52e9a7750876fda3d9c
Stas <stastimofeew98@gmail.com>
2021-12-06 01:17:50 +0300

    add validate and ajax

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,67 @@
 $(document).ready(function(){
+        $('.btn_modal').on('click', function(){
+                $('#modal-wrapper').fadeOut('slow')
+        });
+        //Header scroll
+        var HeaderTop = $('#header').offset().top;
 
-    var HeaderTop = $('#header').offset().top;
-        
-    $(window).scroll(function(){
-            if( $(window).scrollTop() > HeaderTop ) {
-                    $('#header').css({position: 'fixed', top: '0px'});
-            } else {
-                    $('#header').css({position: 'static'});
-            }
+        $(window).scroll(function(){
+                if( $(window).scrollTop() > 800 ) {
+                        $('#header').css({position: 'fixed', top: '0px'});
+                        $('body').css({
+                                'paddingTop': 119 +'px' // делаем отступ у body, равный высоте шапки
+                        });
+                } else {
+                        $('#header').css({position: 'static'});
+                        $('body').css({
+                                'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+                        })
+                }
         });
 
+        //Validate
+        $('#form').validate({
+                rules: {
+                        name: "required",
+                        tel: {
+                                required: true
+                        },
+                        message: {
+                                required: true,
+                                minlength: 20
+                        }
+                },
+                messages: {
+                        name: "Пожалуйста, введите Ваше имя",
+                        tel: {
+                                required: "Пожалуйста, введите Ваш номер телефона"
+                        },
+                        message: {
+                                required: "Пожалуйста, введите сообщение с обращением",
+                                minlength: jQuery.validator.format("Введите не менее {0} символов")
+                        }
+                }
+        });
+
+        //Masked input
+        $('#phone').mask('+7 (000) 000-00-00');
 
 
+        $('form').submit(function(e) {
+                e.preventDefault();
+                $.ajax({
+                        type: "POST",
+                        url: "mailer/smart.php",
+                        data: $(this).serialize()
+                }).done(function() {
+                        $(this).find("input").val("");
+                        $('#modal-wrapper').fadeIn('slow');
 
+                        $('form').trigger('reset');
+                });
+                return false;
+
+        });
 
 
 
